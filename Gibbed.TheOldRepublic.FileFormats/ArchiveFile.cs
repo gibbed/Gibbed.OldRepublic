82a05acdff5fdab1cce73e77482961565ac47f12
Rick Gibbed <gibbed@users.noreply.github.com>
2011-12-12 19:44:30 +0000

    - Add FileFormats, RebuildFileLists. - Add TOR project data, initial file lists.

---
+++ Gibbed.TheOldRepublic.FileFormats/ArchiveFile.cs
@@ -0,0 +1,104 @@
+﻿/* Copyright (c) 2011 Rick (rick 'at' gibbed 'dot' us)
+ * 
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * 
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would
+ *    be appreciated but is not required.
+ * 
+ * 2. Altered source versions must be plainly marked as such, and must not
+ *    be misrepresented as being the original software.
+ * 
+ * 3. This notice may not be removed or altered from any source
+ *    distribution.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Gibbed.IO;
+
+namespace Gibbed.TheOldRepublic.FileFormats
+{
+    public class ArchiveFile
+    {
+        public Endian Endian;
+        public uint Version;
+        public uint DefaultFileTableSize;
+        public uint Unknown18;
+        public uint WriteSequence;
+        public uint ConfirmSequence;
+        public List<Archive.Entry> Entries = new List<Archive.Entry>();
+
+        public void Serialize(Stream output)
+        {
+            throw new FormatException();
+        }
+
+        public void Deserialize(Stream input)
+        {
+            if (input.ReadValueU32(Endian.Little) != 0x0050594D) // 'MYP\0'
+            {
+                throw new FormatException("bad magic");
+            }
+
+            input.Seek(4, SeekOrigin.Current);
+            
+            var marker = input.ReadValueU32(Endian.Little);
+            if (marker != 0xFD23EC43 &&
+                marker.Swap() != 0xFD23EC43)
+            {
+                throw new FormatException("bad marker");
+            }
+            var endian = marker == 0xFD23EC43 ? Endian.Little : Endian.Big;
+
+            input.Seek(-8, SeekOrigin.Current);
+            var version = input.ReadValueU32(endian);
+            //if (version < 4 || version > 5)
+            if (version != 5)
+            {
+                throw new FormatException();
+            }
+            input.Seek(4, SeekOrigin.Current);
+
+            this.Version = version;
+            this.Endian = endian;
+
+            this.Entries.Clear();
+
+            var initialFileTableOffset = input.ReadValueS64(endian);
+            this.DefaultFileTableSize = input.ReadValueU32(endian);
+            this.Unknown18 = input.ReadValueU32(endian);
+            this.WriteSequence = input.ReadValueU32(endian);
+            this.ConfirmSequence = input.ReadValueU32(endian);
+
+            var nextFileTableOffset = initialFileTableOffset;
+            while (nextFileTableOffset != 0)
+            {
+                input.Seek(nextFileTableOffset, SeekOrigin.Begin);
+                var fileTableSize = input.ReadValueU32(endian);
+                nextFileTableOffset = input.ReadValueS64(endian);
+
+                for (uint i = 0; i < fileTableSize; i++)
+                {
+                    var entry = new Archive.Entry();
+                    entry.Offset = input.ReadValueS64(endian);
+                    entry.HeaderSize = input.ReadValueU32(endian);
+                    entry.DataCompressedSize = input.ReadValueU32(endian);
+                    entry.DataUncompressedSize = input.ReadValueU32(endian);
+                    entry.NameHash = input.ReadValueU64(endian);
+                    entry.DataHash = input.ReadValueU32(endian);
+                    entry.Flags = input.ReadValueU16(endian);
+                    this.Entries.Add(entry);
+                }
+            }
+        }
+    }
+}
---
+++ Gibbed.TheOldRepublic.FileFormats/StringHelpers.cs
@@ -0,0 +1,174 @@
+﻿/* Copyright (c) 2011 Rick (rick 'at' gibbed 'dot' us)
+ * 
+ * This software is provided 'as-is', without any express or implied
+ * warranty. In no event will the authors be held liable for any damages
+ * arising from the use of this software.
+ * 
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
+ * freely, subject to the following restrictions:
+ * 
+ * 1. The origin of this software must not be misrepresented; you must not
+ *    claim that you wrote the original software. If you use this software
+ *    in a product, an acknowledgment in the product documentation would
+ *    be appreciated but is not required.
+ * 
+ * 2. Altered source versions must be plainly marked as such, and must not
+ *    be misrepresented as being the original software.
+ * 
+ * 3. This notice may not be removed or altered from any source
+ *    distribution.
+ */
+
+using System.Text;
+
+namespace Gibbed.TheOldRepublic.FileFormats
+{
+    public static class StringHelpers
+    {
+        #region Jenkins
+        private static void HashJenkins(byte[] data, int index, int length, uint seed, out uint pc, out uint pb)
+        {
+            uint a, b, c;
+            a = b = c = 0xDEADBEEF + (uint)length + seed;
+            c += 0; //pb;
+
+            int i = index;
+            while (i + 12 < length)
+            {
+                a += (uint)data[i++] |
+                    ((uint)data[i++] << 8) |
+                    ((uint)data[i++] << 16) |
+                    ((uint)data[i++] << 24);
+                b += (uint)data[i++] |
+                    ((uint)data[i++] << 8) |
+                    ((uint)data[i++] << 16) |
+                    ((uint)data[i++] << 24);
+                c += (uint)data[i++] |
+                    ((uint)data[i++] << 8) |
+                    ((uint)data[i++] << 16) |
+                    ((uint)data[i++] << 24);
+
+                a -= c;
+                a ^= (c << 4) | (c >> (32 - 4));
+                c += b;
+                b -= a;
+                b ^= (a << 6) | (a >> (32 - 6));
+                a += c;
+                c -= b;
+                c ^= (b << 8) | (b >> (32 - 8));
+                b += a;
+                a -= c;
+                a ^= (c << 16) | (c >> (32 - 16));
+                c += b;
+                b -= a;
+                b ^= (a << 19) | (a >> (32 - 19));
+                a += c;
+                c -= b;
+                c ^= (b << 4) | (b >> (32 - 4));
+                b += a;
+            }
+
+            if (i < length)
+            {
+                a += data[i++];
+            }
+
+            if (i < length)
+            {
+                a += (uint)data[i++] << 8;
+            }
+
+            if (i < length)
+            {
+                a += (uint)data[i++] << 16;
+            }
+
+            if (i < length)
+            {
+                a += (uint)data[i++] << 24;
+            }
+
+            if (i < length)
+            {
+                b += (uint)data[i++];
+            }
+
+            if (i < length)
+            {
+                b += (uint)data[i++] << 8;
+            }
+
+            if (i < length)
+            {
+                b += (uint)data[i++] << 16;
+            }
+
+            if (i < length)
+            {
+                b += (uint)data[i++] << 24;
+            }
+
+            if (i < length)
+            {
+                c += (uint)data[i++];
+            }
+
+            if (i < length)
+            {
+                c += (uint)data[i++] << 8;
+            }
+
+            if (i < length)
+            {
+                c += (uint)data[i++] << 16;
+            }
+
+            if (i < length)
+            {
+                c += (uint)data[i++] << 24;
+            }
+
+            c ^= b;
+            c -= (b << 14) | (b >> (32 - 14));
+            a ^= c;
+            a -= (c << 11) | (c >> (32 - 11));
+            b ^= a;
+            b -= (a << 25) | (a >> (32 - 25));
+            c ^= b;
+            c -= (b << 16) | (b >> (32 - 16));
+            a ^= c;
+            a -= (c << 4) | (c >> (32 - 4));
+            b ^= a;
+            b -= (a << 14) | (a >> (32 - 14));
+            c ^= b;
+            c -= (b << 24) | (b >> (32 - 24));
+
+            pc = c;
+            pb = b;
+        }
+
+        private static void HashJenkins(this string input, out uint pc, out uint pb)
+        {
+            var data = Encoding.ASCII.GetBytes(input);
+            HashJenkins(data, 0, data.Length, 0, out pc, out pb);
+        }
+
+        public static uint HashJenkins32(this string input)
+        {
+            var data = Encoding.ASCII.GetBytes(input);
+            uint pc, pb;
+            input.HashJenkins(out pc, out pb);
+            return pc;
+        }
+
+        public static ulong HashJenkins64(this string input)
+        {
+            var data = Encoding.ASCII.GetBytes(input);
+            uint pc, pb;
+            input.HashJenkins(out pc, out pb);
+            return ((ulong)pc << 0) | ((ulong)pb << 32);
+        }
+        #endregion
+    }
+}
